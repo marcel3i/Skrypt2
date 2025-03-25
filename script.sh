c78be8401098d01be6d9fc1bcb7eb7cd64e24967
marcel3i <marcel.kitlowski@gmail.com>
2025-03-25 19:23:39 +0100

    Script verison 2

--- script.sh
+++ script.sh
@@ -1,70 +1,61 @@
-#!/bin/bash -f 
+#!/bin/bash -f
 
-fileName=""
-directory=""
-sizeOption=""
-mtimeOption=""
+DIRECTORY_NAME=""
+FILE_NAME=""
+SIZE_OPTION=""
+TIME_OPTION=""
+CONTENT_OPTION=""
 
 while true; do
-    clear
-    echo "1. Nazwa pliku: $fileName"
-    echo "2. Katalog: $directory"
-    echo "3. Rozmar pliku (format: +100k, -2M): $sizeOption"
-    echo "4. Data modyfikacji (format: -7, +25): $mtimeOption"
-    echo "5. Szukaj"
-    echo "6. Koniec"
-    echo
-    read -p "Wybierz opcje (1-6): " choice
-
-    case $choice in
-        1)
-            read -p "Podaj nazwe pliku (np. *sh): " fileName
-            ;;
-        2)
-            read -p "Podaj katalog pliku (domyślnie domowy): " directory
-            if [[ "$directory" != /* ]]; then
-                directory="$HOME/$directory"
-            fi
-            ;;
-        3)
-            read -p "Podaj rozmiar pliku (format: +100k, -2M): " sizeOption
-            ;;
-        4)
-            read -p "Podaj date modyfikacji pliku (np. -7, +30): " mtimeOption
-            ;;
-        5) 
-            if [ -z "$fileName" ]; then
-                fileName="*"
-            fi
-            if [ -z "$directory" ]; then
-                directory="$HOME"
-            fi
-            cmd="find \"$directory\" -name \"$fileName\""
-            [ -n "$sizeOption" ] && cmd+=" -size $sizeOption"
-            [ -n "$mtimeOption" ] && cmd+=" -mtime $mtimeOption"
-
-            echo
-            echo "Komenda: $cmd"
-            echo
-            echo "Wynik wyszukiwania: "
-            eval $cmd
-
-            if [ "$fileName" = "*" ]; then
-                fileName=""
-            fi
-            if [ "$directory" = "$HOME" ]; then
-                directory=""
-            fi
-
-            echo
-            read -p "Wciśnij enter, by wrócić do menu..." 
-            ;;
-        6)
-            echo "Zakoczono dzialanie skryptu"
-            break
-            ;;
-        *)
-            echo "Nieprawidlowy input"
-            ;;
-    esac
+	clear
+	echo "Wybierz opcje"
+	echo "1. Nazwa pliku: $FILE_NAME"
+	echo "2. Nazwa katalogui: $DIRECTORY_NAME"
+	echo "3. Rozmiar pliku (format +100l, -2M): $SIZE_OPTION"
+	echo "4. Data modfyikacji (format: -7, +25): $TIME_OPTION"
+	echo "5. Zawartosc pliku: $CONTENT_OPTION"
+	echo "6. Szukaj"
+	echo "7. Koniec"
+	read -p "Wybierz opcje (1-7): " CHOICE
+	case $CHOICE in
+	1)
+	  read -p "Podaj nazwe pliku: " FILE_NAME
+	  ;;
+	2)
+	  read -p "Podaj nazwe katalogu: " DIRECTORY_NAME
+	  ;;
+	3)
+	  read -p "Podaj rozmiar pliku: " SIZE_OPTION
+	  ;;
+	4)
+	  read -p "Podaj date mofycikacji: " TIME_OPTION
+	  ;;
+	5)
+	  read -p "Podaj zawartosc pliku: " CONTENT_OPTION
+	  ;;
+	6)
+	 echo "Szukanie ..."
+	 echo 
+	  cmd="find \"$DIRECTORY_NAME\" -name \"$FILE_NAME\""
+	  cmd+=" -size $SIZE_OPTION"
+	  cmd+=" -mtime $TIME_OPTION"
+	  cmd+=" -exec grep \"$CONTENT_OPTION\" {} \;"
+	 #echo "Komenda: $cmd"
+	 RESULT=$(eval $cmd)
+	 #echo $RESULT
+	 if [ -z "${RESULT}" ]; then
+	  echo "Plik nie istnieje"
+	 else 
+	  echo "Plik istnieje" 
+	 fi
+	 read -p "Wcisnij enter by wrocic do menu"
+	 ;;
+	7)
+	  echo "Koniec programu"
+	  break;
+	 ;;
+	*)
+	  echo "Bledny input"
+	  ;;
+	esac
 done
